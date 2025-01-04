02f1262578b99d25186911e5c26963d81ce4a571
Vittoria Condorelli <vittoriacondorelli005@gmail.com>
2025-01-04 18:40:15 +0100

    adding PCR3BP data

--- main.cpp
+++ main.cpp
@@ -5,6 +5,7 @@
 #include "NormalFormFinder/NormalFormFinder.cpp"
 using namespace std;
 using namespace capd;
+using capd::autodiff::Node;
 
 #define MAX_DERIVATIVE 10
 #define METHOD_DEGREE 2
@@ -77,10 +78,62 @@ void henon_heiles_test()
     PseudoNormalForm normalForm = finder.calculatePseudoNormalForm();
 }
 
+void pcr3bpVectorField(Node /*t*/, Node in[], int /*dimIn*/, Node out[], int /*dimOut*/, Node params[], int /*noParams*/)
+{
+    Node mu = params[0]; // relative mass of Jupiter
+    Node mj = 1 - mu; // relative mass of the Sun
+
+    // in[0] = x, in[1] = vx, in[2] = y, in[3] = vy
+    Node xMu = in[0] + mu;
+    Node xMj = in[0] - mj;
+    Node ySquare = in[2]*in[2];
+    Node xMuSquareYSquare = xMu*xMu + ySquare;
+    Node xMjSquareYSquare = xMj*xMu + ySquare;
+    Node factor1 = mj / ( xMuSquareYSquare * sqrt(xMuSquareYSquare) );
+    Node factor2 = mu / ( xMjSquareYSquare * sqrt(xMjSquareYSquare) );
+
+    out[0] = in[1];
+    out[1] = in[0] - xMu*factor1 - xMj*factor2 + 2*in[3];
+    out[2] = in[3];
+    out[3] = in[2]*(1 - factor1 - factor2) - 2*in[1];
+}
+
+// TODO: not working because of sqrt?
+void PCR3BP_test() 
+{
+    int dim=4, noParam=1;
+    CMap f(pcr3bpVectorField, dim, dim, noParam, MAX_DERIVATIVE);
+    f.setParameter(0, 0.5); // mu parameter
+
+    CVector p({0, 0, -0.866025, 0}); // L4
+
+    CMatrix lambda({ 
+        {4.46783, 0, 0, 0}, 
+        {0, -4.46783, 0, 0}, 
+        {0, 0, Complex(0, 2.44161), 0}, 
+        {0, 0, 0, Complex(0, -2.44161)} });
+
+    CMatrix J({ 
+        {0.655789, 0.17235, -0.0894944, 0.0571208} ,
+        {0.655789, -0.17235, 0.0894944, 0.0571208}, 
+        {-0.655789, Complex(0, -0.0941873), Complex(0, 1.26971), 0.442879},
+        {-0.655789, Complex(0, 0.0941873), Complex(0, -1.26971), 0.442879} });
+
+    CMatrix invJ({ 
+        {0.67534, 0.67534, -0.0871, -0.0871}, 
+        {3.0173, -3.0173, Complex(0, -0.21267), Complex(0, 0.21267)}, 
+        {0.22382, -0.22382, Complex(0, -0.40957), Complex(0, 0.40957)}, 
+        {1, 1, 1, 1} });
+
+    NormalFormFinder<Logger<Diagnostic>> finder(METHOD_DEGREE, f, p, lambda, J, invJ);
+    PseudoNormalForm normalForm = finder.calculatePseudoNormalForm();
+}
+
 int main()
 {
     diagonal_matrix_test();
     henon_heiles_test();
+    // PCR3BP_test();
 
     return 0;
 }
